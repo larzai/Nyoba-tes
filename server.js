ffcc573b1d1686b4440c658d2d941378c2d6d22b
Larzai <147675083+larzai@users.noreply.github.com>
2025-01-26 10:49:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,38 +16,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'web', 'index.html'));
 });
 
-app.use('/managminpagesdashboard', (req, res, next) => {
-    const userAgent = req.headers['user-agent'];
-    const authCookie = req.cookies.authCode;
-
-    if (!userAgent.includes('Chrome')) {
-        return res.send(`
-            <script>
-                alert('Anda harus menggunakan browser Chrome untuk mengakses halaman ini!');
-                window.history.back();
-            </script>
-        `);
-    }
-
-    if (!authCookie || authCookie !== 'aaacdbdasnak298#7b$88+owohs') {
-        return res.send(`
-            <script>
-                alert('Jangan masuk!');
-                window.history.back();
-            </script>
-        `);
-    }
-    next();
-});
-
-app.get('/lymed/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'lymed', 'index.html'));
-});
-
-app.get('/lymed', (req, res) => {
-    res.sendFile(path.join(__dirname, 'lymed', 'index.html'));
-});
-
 app.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
 });
